06621c5ab1a883ce91c8d9509b39da78371a6286
vis2k <info@noobtuts.com>
2021-07-28 01:41:52 +0800

    ArraySegmentX project and first tests

---
+++ ArraySegmentX/ArraySegmentX.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArraySegmentX
+{
+    class ArraySegmentX
+    {
+    }
+}
---
+++ ArraySegmentX/Tests.cs
@@ -0,0 +1,42 @@
+using System;
+using NUnit.Framework;
+
+namespace ArraySegmentX
+{
+    public class Tests
+    {
+        byte[] bytes = new byte[1000];
+
+        [Test]
+        [TestCase(1_000_000)] // 1.5s
+        public void ByteArrayTest(int iterations)
+        {
+            // ArraySegment always uses Array[Array.Offset + i].
+            // let's use an offset here too for fairness.
+            int offset = 0;
+            for (int i = 0; i < iterations; ++i)
+            {
+                for (int j = 0; j < bytes.Length; ++j)
+                {
+                    int n = bytes[offset + j];
+                }
+            }
+        }
+
+        [Test]
+        [TestCase(1_000_000)] // 3.6s
+        public void ArraySegmentTest(int iterations)
+        {
+            // ArraySegment always uses Array[Array.Offset + i].
+            // let's use an offset here too for fairness.
+            ArraySegment<byte> segment = new ArraySegment<byte>(bytes);
+            for (int i = 0; i < iterations; ++i)
+            {
+                for (int j = 0; j < bytes.Length; ++j)
+                {
+                    int n = segment.Array[segment.Offset + j];
+                }
+            }
+        }
+    }
+}
