07aaec5a0f2d88608b87aa63df356cb3bf439a70
vis2k <info@noobtuts.com>
2021-07-28 01:54:21 +0800

    add IL code comments

--- ArraySegmentX/Tests.cs
+++ ArraySegmentX/Tests.cs
@@ -18,6 +18,13 @@ namespace ArraySegmentX
             {
                 for (int j = 0; j < bytes.Length; ++j)
                 {
+                    // IL_000d: ldarg.0      // this
+                    // IL_000e: ldfld        unsigned int8[] ArraySegmentX.Tests::bytes
+                    // IL_0013: ldloc.0      // offset
+                    // IL_0014: ldloc.2      // j
+                    // IL_0015: add
+                    // IL_0016: ldelem.u1
+                    // IL_0017: stloc.3
                     int n = bytes[offset + j];
                 }
             }
@@ -34,6 +41,14 @@ namespace ArraySegmentX
             {
                 for (int j = 0; j < bytes.Length; ++j)
                 {
+                    // IL_0018: ldloca.s     segment
+                    // IL_001a: call         instance !0/*unsigned int8*/[] valuetype [System.Runtime]System.ArraySegment`1<unsigned int8>::get_Array()
+                    // IL_001f: ldloca.s     segment
+                    // IL_0021: call         instance int32 valuetype [System.Runtime]System.ArraySegment`1<unsigned int8>::get_Offset()
+                    // IL_0026: ldloc.2      // j
+                    // IL_0027: add
+                    // IL_0028: ldelem.u1
+                    // IL_0029: stloc.3
                     int n = segment.Array[segment.Offset + j];
                 }
             }
@@ -50,6 +65,14 @@ namespace ArraySegmentX
             {
                 for (int j = 0; j < bytes.Length; ++j)
                 {
+                    // IL_0018: ldloc.0      // segment
+                    // IL_0019: ldfld        !0/*unsigned int8*/[] valuetype ArraySegmentX`1<unsigned int8>::Array
+                    // IL_001e: ldloc.0      // segment
+                    // IL_001f: ldfld        int32 valuetype ArraySegmentX`1<unsigned int8>::Offset
+                    // IL_0024: ldloc.2      // j
+                    // IL_0025: add
+                    // IL_0026: ldelem.u1
+                    // IL_0027: stloc.3      // n
                     int n = segment.Array[segment.Offset + j];
                 }
             }
