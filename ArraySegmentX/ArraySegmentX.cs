d454bfbe369911119cdf0527d92cf3253e000ac3
vis2k <info@noobtuts.com>
2021-07-28 01:49:21 +0800

    ArraySegmentX so far

--- ArraySegmentX/ArraySegmentX.cs
+++ ArraySegmentX/ArraySegmentX.cs
@@ -1,8 +1,31 @@
 ﻿using System;
 
-namespace ArraySegmentX
+public struct ArraySegmentX<T>
 {
-    class ArraySegmentX
+    public ArraySegmentX(T[] array)
     {
+        if (array == null) throw new ArgumentNullException("array");
+
+        Array = array;
+        Offset = 0;
+        Count = array.Length;
     }
+
+    public ArraySegmentX(T[] array, int offset, int count)
+    {
+        if (array == null) throw new ArgumentNullException("array");
+        if (offset < 0) throw new ArgumentOutOfRangeException("offset");
+        if (count < 0) throw new ArgumentOutOfRangeException("count");
+        if (array.Length - offset < count) throw new ArgumentException("Length - offset needs to be >= count");
+
+        Array = array;
+        Offset = offset;
+        Count = count;
+    }
+
+    public T[] Array { get; }
+    public int Offset { get; }
+    public int Count { get; }
+
+    // TODO equals, gethashcode, etc.
 }
--- ArraySegmentX/Tests.cs
+++ ArraySegmentX/Tests.cs
@@ -38,5 +38,21 @@ namespace ArraySegmentX
                 }
             }
         }
+
+        [Test]
+        [TestCase(1_000_000)] // 5.2s
+        public void ArraySegmentXTest(int iterations)
+        {
+            // ArraySegment always uses Array[Array.Offset + i].
+            // let's use an offset here too for fairness.
+            ArraySegmentX<byte> segment = new ArraySegmentX<byte>(bytes);
+            for (int i = 0; i < iterations; ++i)
+            {
+                for (int j = 0; j < bytes.Length; ++j)
+                {
+                    int n = segment.Array[segment.Offset + j];
+                }
+            }
+        }
     }
 }
